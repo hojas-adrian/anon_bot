a13decf5bd98c6f503d1e3e669c36277fd3f24b0
Adrián Hojas Moré <hojas.adrian@outlook.com>
2024-08-01 08:37:17 -0400

    log fwd

--- src/handlers/on_log.ts
+++ src/handlers/on_log.ts
@@ -1,7 +1,20 @@
 import { Context } from "../deps.ts";
-import { LOG_GROUP } from "../helpers/constants.ts";
+import { GROUP_ID, LOG_GROUP } from "../helpers/constants.ts";
 
 export default async (ctx: Context) => {
+  if (ctx.message?.reply_to_message) {
+    try {
+      await ctx.api.sendMessage(LOG_GROUP, "fwd");
+      await ctx.api.forwardMessage(
+        LOG_GROUP,
+        +GROUP_ID,
+        ctx.message.reply_to_message.message_id
+      );
+    } catch {
+      await ctx.api.sendMessage(LOG_GROUP, "ams");
+    }
+  }
+
   try {
     await ctx.forwardMessage(LOG_GROUP);
   } catch {
